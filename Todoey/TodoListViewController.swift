207c111249eaacf23089f1c4d4ca97ee51cd189b
Andrzej Stojak <andrzej.stojak@gmail.com>
2018-12-28 19:36:27 +0100

    First commit, Todo view controller

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Andrzej Stojak on 22/12/2018.
+//  Copyright © 2018 Andrzej Stojak. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["buy bike","make shop","learn to code"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+}
+
